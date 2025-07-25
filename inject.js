2b91834406131cdf76e72705a12320880f4731c0
Ali Baghernia <alibaghernia0@gmail.com>
2025-07-26 01:56:07 +0330

    add more short keys

--- inject.js
+++ inject.js
@@ -18,28 +18,38 @@ document.addEventListener('keydown', (e) => {
         return;
     }
 
+    if (e.code === 'KeyC') {
+        window.open('https://chatgpt.com/');
+        return;
+    }
+
+    if (e.code === 'KeyD') {
+        window.open('https://chat.deepseek.com/');
+        return;
+    }
+
     if (e.shiftKey && e.code === 'KeyM') {
         window.open('https://www.google.com/maps');
         return;
     }
 
-    // if (e.code === 'KeyL') {
-    //     window.open('https://www.ldoceonline.com/');
-    //     return;
-    // }
+    if (e.code === 'KeyL') {
+        window.open('https://www.ldoceonline.com/');
+        return;
+    }
 
-    // if (e.code === 'KeyY') {
-    //     window.open('');
-    //     return;
-    // }
+    if (e.code === 'KeyY') {
+        window.open('https://www.youtube.com/');
+        return;
+    }
 
     // if (e.code === 'Digit1') {
     //     window.open('');
     //     return;
     // }
 
-    // if (e.code === 'KeyT') {
-    //     window.open('https://translate.google.com/');
-    //     return;
-    // }
+    if (e.code === 'KeyT') {
+        window.open('https://translate.google.com/?sl=en&tl=fa&op=translate');
+        return;
+    }
 })
